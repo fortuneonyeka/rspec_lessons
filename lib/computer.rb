d0704d13242f17a7cc4965ef1e4ac0eeee24a351
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-20 17:24:55 +0100

    refactor the test

--- lib/computer.rb
+++ lib/computer.rb
@@ -1,13 +1,29 @@
 
 class Computer
-  def initialize(username, password)
+
+  # def initialize(username, password)
+  #   @username = username
+  #   @password = password
+  # end
+
+  def user_id
     if @username == username
       return true
     end
     false
+    # if @password == password
+    #   return true
+    # end
+    # false
+  end
+  def user_pass
     if @password == password
       return true
     end
     false
+    # if @password == password
+    #   return true
+    # end
+    # false
   end
 end
--- spec/computer_spec.rb
+++ spec/computer_spec.rb
@@ -2,9 +2,14 @@ require 'computer'
 
 
 describe "Computer" do
-  it "creates file username and Password " do
-   user = Computer.new("Fortune", "1256782")
-    # result = my_computer.create("Fortune", "123456")
+  it "creates file username" do
+   user = Computer.new
+    expect(user).to be_truthy
+    end
+  end
+describe "Computer" do
+  it "creates file password" do
+   user = Computer.new
     expect(user).to be_truthy
     end
   end
