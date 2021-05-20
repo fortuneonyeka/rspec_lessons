1ac8be170461117d4b8c5ee10ebdbfef8dc691c7
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2021-05-20 16:48:44 +0100

    update spec file with computer_spec.rb

---
+++ lib/computer.rb
@@ -0,0 +1,14 @@
+
+class Computer
+  def initialize(username, password)
+    if @username == username
+      return true
+    end
+    false
+    if @password == password
+      return true
+    end
+    false
+  end
+end
+  
